e72ee99161fac7bf0f88ea8f7a2a1d8fdd2b22bb
Aleksander Peichev <alex@dimiroma.com>
2019-03-21 16:39:40 +0200

    add

--- src/consoleConvertor.java
+++ src/consoleConvertor.java
@@ -5,14 +5,7 @@ public class consoleConvertor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       double sumConvert = Double.parseDouble(scanner.nextLine());
-       String inCurrancy = scanner.nextLine();
-       String outCurrancy = scanner.nextLine();
 
-       double bgn = 1;
-       double usd = bgn * 1.79549;
-       double eur = bgn * 1.95583;
-       double gbp = bgn * 2.53405;
 
 
 
--- src/dailyEarnings.java
+++ src/dailyEarnings.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class dailyEarnings {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int monthDays = Integer.parseInt(scanner.nextLine());
+        if (monthDays < 5 || monthDays > 30){
+            System.out.println("failure monthDays");
+            return;
+        }
+        double earnedMoneyPerDay = Double.parseDouble(scanner.nextLine());
+        if (earnedMoneyPerDay < 10.00 || earnedMoneyPerDay > 2000.00){
+            System.out.println("failure earnedMoneyPerDay");
+            return;
+        }
+
+        double usdExchangeRate =  Double.parseDouble(scanner.nextLine());
+        if (usdExchangeRate < 0.99 || usdExchangeRate > 1.99){
+            System.out.println("failure usdEchangeDay");
+            return;
+        }
+
+        double monthSallary = monthDays * earnedMoneyPerDay;
+        double yearEarningsUSD = monthSallary * 12 + monthSallary * 2.5;
+
+        double bills = yearEarningsUSD * 0.25;
+
+        double clearYearEarningsUSD = yearEarningsUSD - bills;
+        double clearYearEarningsBGN = clearYearEarningsUSD * usdExchangeRate;
+
+        double averageEarningPerDay = clearYearEarningsBGN / 365;
+
+        System.out.printf("Average earning per day: %.2f", averageEarningPerDay);
+
+
 
     }
 }
